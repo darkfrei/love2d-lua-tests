deedc4032543d7d33d2dfaec125a56d17c33b5dd
darkfrei <34188677+darkfrei@users.noreply.github.com>
2021-07-01 23:22:35 +0200

    Field of view, FOV like in roguelike games

---
+++ field-of-view/fov.lua
@@ -0,0 +1,46 @@
+fov = {}
+-- i and j - number of columns and rows, integers
+-- x and y - part
+
+
+
+fov.march_line = function (map, view, i1, j1, i2, j2) -- source and target
+	local length = math.abs(i2-i1) + math.abs(j2-j1)
+	if length > 1 then
+		local dx = (i2-i1)/(length)
+		local dy = (j2-j1)/(length)
+		for n = 1, length do
+			local i, j = math.floor (i1+n*dx+0.5), math.floor (j1+n*dy+0.5)
+			
+			view[i] = view[i] or {}
+			if map[i] and map[i][j] and map[i][j] <= 0 then
+				-- is opaque
+				view[i][j] = false
+				return
+			else
+				-- is transparent
+				view[i][j] = true
+			end
+		end
+	else
+		if map[i2] and map[i2][j2] and map[i2][j2] <= 0 then
+			view[i2] = view[i2] or {}
+			view[i2][j2] = false
+		end
+	end
+end
+
+function fov.marching (map, i, j, radius)
+	local view = {}
+	for i2 = i - radius, i + radius do
+		fov.march_line (map, view, i, j, i2, j+radius)
+		fov.march_line (map, view, i, j, i2, j-radius)
+	end
+	for j2 = j - radius + 1, i + radius - 1 do
+		fov.march_line (map, view, i, j, i+radius, j2)
+		fov.march_line (map, view, i, j, i-radius, j2)
+	end
+	return view
+end
+
+return fov
---
+++ field-of-view/main.lua
@@ -0,0 +1,76 @@
+-- License CC0 (Creative Commons license) (c) darkfrei, 2021
+
+local fov = require ('fov')
+
+function love.load()
+	local ww, wh = love.window.getDesktopDimensions( displayindex )
+	love.window.setMode( ww/2, wh/2)
+	
+	width, height = love.graphics.getDimensions( )
+
+	map = {}
+	rez = 10
+	max_i, max_j = math.floor(width/rez), math.floor(height/rez)
+	for i = 1, max_i do
+		map[i] = {}
+		for j = 1, max_j do
+			if math.random (100) == 1 then
+				map[i][j] = 0 -- 0 is wall, black
+			else
+				map[i][j] = 1 -- 1 is space, white
+			end
+		end
+	end
+	
+	player = {i=math.floor(max_i/2), j=math.floor(max_j/2)}
+	radius = max_j
+end
+
+ 
+function love.update(dt)
+	
+end
+
+
+function love.draw()
+	-- draw map
+	for i, js in pairs (map) do
+		for j, value in pairs (js) do
+			local c = (value+1)/3
+			love.graphics.setColor (c,c,c)
+			love.graphics.rectangle('fill', rez*(i-1), rez*(j-1), rez, rez)
+		end
+	end
+	
+	-- get and draw fov
+	local view = fov.marching (map, player.i, player.j, radius)
+	for i, js in pairs (view) do
+		for j, value in pairs (js) do
+			local c = value and 1 or 0
+			love.graphics.setColor (c,c,1-c)
+			love.graphics.rectangle('fill', rez*(i-1), rez*(j-1), rez, rez)
+		end
+	end
+	
+	-- draw player
+	love.graphics.setColor (0,1,0)
+	love.graphics.rectangle('fill', rez*(player.i-1), rez*(player.j-1), rez, rez)
+	
+	-- GUI
+	love.graphics.setColor (1,1,1)
+	love.graphics.print('press WASD to move green dot')
+end
+
+function love.keypressed(key, scancode, isrepeat)
+	if key == "d" then
+		player.i = player.i + 1
+	elseif key == "s" then
+		player.j = player.j + 1
+	elseif key == "a" then
+		player.i = player.i - 1
+	elseif key == "w" then
+		player.j = player.j - 1
+	elseif key == "escape" then
+		love.event.quit()
+	end
+end
