ce1224c76dd30604e6c0e26c20ccddd3cae0ee0b
darkfrei <34188677+darkfrei@users.noreply.github.com>
2021-08-09 22:53:05 +0200

    Buttons are clickable (2)

--- main-menu/menu.lua
+++ main-menu/menu.lua
@@ -9,8 +9,9 @@ local yellow = {1, 220/255, 60/255}
 
 local fonts = {}
 
+menu.background_color = black
 
-menu.buttons = {
+menu.main_buttons = {
 	{
 		name = "new",
 		text = "New game",
@@ -62,6 +63,87 @@ menu.buttons = {
 	},
 }
 
+
+menu.settings_buttons = {
+	{
+		name = "change_color_white",
+		text = "White",
+		x = 80/800,
+		y = 180/600,
+		w = 280/800,
+		h = 50/600
+	},
+	{
+		name = "change_color_black",
+		text = "Black",
+		x = 420/800,
+		y = 180/600,
+		w = 280/800,
+		h = 50/600
+	},
+	{
+		name = "change_color_gray_70",
+		text = "Gray 70",
+		x = 80/800,
+		y = 240/600,
+		w = 280/800,
+		h = 50/600
+	},
+	{
+		name = "change_color_gray_90",
+		text = "Gray 90",
+		x = 420/800,
+		y = 240/600,
+		w = 280/800,
+		h = 50/600
+	},
+	{
+		name = "go_to_main_menu",
+		text = "Back",
+		x = 150/800,
+		y = 320/600,
+		w = 500/800,
+		h = 50/600
+	},
+}
+
+menu.credits_buttons = {
+	{
+		name = "text-field",
+		text = [[Many thanks to
+all Löve creators, 
+specially for every
+and every
+and every
+and every
+and every
+and every
+and every
+and every
+and every
+and every
+and every
+and every
+and another good people]],
+		x = 150/800,
+		y = 180/600,
+		w = 500/800,
+		h = 280/600
+	},
+	
+	{
+		name = "go_to_main_menu",
+		text = "Back",
+		x = 150/800,
+		y = 480/600,
+		w = 500/800,
+		h = 50/600
+	},
+}
+
+
+menu.buttons = menu.main_buttons
+
 menu.logo = {
 	x = 150/800,
 	y = 20/600,
@@ -79,23 +161,33 @@ function menu.update()
 	
 end
 
-function get_right_font (button, max_w, max_h)
-	local font = love.graphics.newFont(10)
+function get_right_font (button, max_w, max_h, max_h2)
+	local font = love.graphics.newFont(5)
 	
 	local text = button.text
 	
 	local tw = font:getWidth (text)
 	local th = font:getHeight()
+	local width, wrappedtext = font:getWrap( text, max_w)
+	local n_lines = #wrappedtext
 	
 	for i = 1, 100 do
 		
 		local newfont = love.graphics.newFont(th+i)
 		if newfont:getWidth (text) > max_w then
+			-- not enough width
 			button.text_size = th + i-1
 			local prev_font = love.graphics.newFont(th+i-1)
 --			print (prev_font:getWidth (text), prev_font:getHeight())
 			return prev_font
-		elseif th+i > max_h then
+		elseif n_lines > 1 and (th+i) > (max_h2-3*(th+i+1))/(n_lines) then
+			-- multiline, not enough height
+			button.text_size = th + i-1
+			local prev_font = love.graphics.newFont(th+i-1)
+--			print (prev_font:getWidth (text), prev_font:getHeight())
+			return prev_font
+		elseif (th+i) > max_h then
+			-- one line, not enough height
 			button.text_size = th + i-1
 			local prev_font = love.graphics.newFont(th+i-1)
 --			print (prev_font:getWidth (text), prev_font:getHeight())
@@ -107,15 +199,19 @@ end
 
 function draw_button_text (x, y, w, h, button)
 	if not button.font then
-		button.font = get_right_font (button, w, 0.6*h)
+		button.font = get_right_font (button, w, 0.6*h, h)
 	end
 	
 	local text = button.text
 	local font = button.font
 	local tw = font:getWidth (text)
 	local th = font:getHeight()
+	local width, wrappedtext = font:getWrap( text, w )
+--	print (text, tw, th, width, #wrappedtext)
+
 	love.graphics.setFont( font )
-	love.graphics.print( text, x+w/2-tw/2, y+h/2-th/2, 0, 1, 1, 0, 0)
+--	love.graphics.print( text, x+w/2-tw/2, y+h/2-th/2, 0, 1, 1, 0, 0)
+	love.graphics.print( text, x+w/2-tw/2, y+h/2+th*(-#wrappedtext)/2, 0, 1, 1, 0, 0)
 end
 
 function is_button_selected (x, y, w, h, mx, my)
@@ -136,7 +232,7 @@ end
 
 function menu.draw()
 	
-	
+	love.graphics.setBackgroundColor (menu.background_color)
 	
 --	love.graphics.setColor(black)
 	local width, height = love.graphics.getDimensions( )
@@ -149,14 +245,15 @@ function menu.draw()
 		local y = math.floor (button.y*height)
 		local w = math.floor (button.w*width)
 		local h = math.floor (button.h*height)
-		if is_button_selected (x, y, w, h, mx, my) then
+		if is_button_selected (x, y, w, h, mx, my) and not (button.name == "text-field") then
 --			love.graphics.setColor(gray_90)
 			love.graphics.setColor(yellow)
 			love.graphics.rectangle('fill', x, y, w, h)
 			love.graphics.setColor(black)
 			draw_button_text (x, y, w, h, button)
 		else
---			love.graphics.setColor(white)
+			love.graphics.setColor(black)
+			love.graphics.rectangle('fill', x, y, w, h)
 			love.graphics.setColor(yellow)
 			love.graphics.rectangle('line', x, y, w, h)
 			love.graphics.setColor(yellow)
@@ -182,6 +279,20 @@ function menu.mousepressed(x, y, button, istouch, presses)
 		if is_button_selected (bx, by, bw, bh, x, y) then
 			if button.name == 'exit' then
 				love.event.quit()
+			elseif button.name == 'settings' then
+				menu.buttons = menu.settings_buttons
+			elseif button.name == 'change_color_white' then
+				menu.background_color = white
+			elseif button.name == 'change_color_black' then
+				menu.background_color = black
+			elseif button.name == 'change_color_gray_70' then
+				menu.background_color = gray_70
+			elseif button.name == 'change_color_gray_90' then
+				menu.background_color = gray_90
+			elseif button.name == 'credits' then
+				menu.buttons = menu.credits_buttons
+			elseif button.name == 'go_to_main_menu' then
+				menu.buttons = menu.main_buttons
 			end
 		end
 	end
