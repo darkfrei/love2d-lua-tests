d8ed199dcc599a4e59e5ffc17668b3c34799a098
darkfrei <34188677+darkfrei@users.noreply.github.com>
2021-08-21 23:27:43 +0200

    Add files via upload

--- empty-main.lua
+++ empty-main.lua
@@ -1,13 +1,46 @@
+-- License CC0 (Creative Commons license) (c) darkfrei, 2021
+
 function love.load()
+	local ddwidth, ddheight = love.window.getDesktopDimensions( display )
+	if ddheight > 1080 then
+		print('ddheight: ' .. ddheight)
+		love.window.setMode(1920, 1080, {resizable=true, borderless=false})
+	else
+		love.window.setMode(ddwidth, ddheight-200, {resizable=true, borderless=false})
+	end
+	width, height = love.graphics.getDimensions( )
+
 	
 end
- 
+
  
 function love.update(dt)
 	
 end
- 
- 
+
+
 function love.draw()
 	
+end
+
+function love.keypressed(key, scancode, isrepeat)
+	if false then
+	elseif key == "escape" then
+		love.event.quit()
+	end
+end
+
+function love.mousepressed( x, y, button, istouch, presses )
+	if button == 1 then -- left mouse button
+	elseif button == 2 then -- right mouse button
+	end
+end
+
+function love.mousemoved( x, y, dx, dy, istouch )
+end
+
+function love.mousereleased( x, y, button, istouch, presses )
+	if button == 1 then -- left mouse button
+	elseif button == 2 then -- right mouse button
+	end
 end
