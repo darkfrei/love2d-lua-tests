e10275b9a35368393c1d52f19a2f9ced6a3ddd69
darkfrei <34188677+darkfrei@users.noreply.github.com>
2021-12-27 00:13:27 +0100

    Add files via upload

--- svg2lua/main.lua
+++ svg2lua/main.lua
@@ -4,79 +4,231 @@
 
 
 local svg2lua = require ('svg2lua')
+local mr = require ('multiresolution')
+love.window.setMode(1920, 1080, {resizable=true, borderless=false})
+mr:load ()
 
-local ds = {
-	"M40,360H-40", -- line
-	"M -40,480 H 40", -- line
-	"M 840,1000 V 920",
-	"M 720,920 V 1000",
-	"M 1280,-40 1200,40",
-	"M 1320,40 1400,-40",
-	"M 40,480 H 360",
-	"M 760,360 400,360",
-	"M 1120,240 1320,40",
-	"M 400,360 40,360",
-	
-	"M 840,920 C 840,680 1040,320 1120,240",
-	"M 1200,40 C 1160,80 1120,120 1080,160",
-	"M 1080,160 C 920,320 520,360 400,360",
-	"M 360,480 C 520,480 720,760 720,920",
-	"M 760,360 C 640,360 560,520 640,600",
-	"M 640,600 C 720,680 840,640 880,560",
-	"M 880,560 C 920,480 880,360 760,360",
-	"M 1080,160 C 1040,200 920,360 760,360",
-	"M 360,480 C 480,480 560,520 640,600",
-	"M 640,600 C 720,680 720,840 720,920",
-	"M 840,920 C 840,800 840,640 880,560",
-	"M 880,560 C 920,480 1120,240 1120,240",
-	
-	"M 0,600 H 360 L 600,840 V 960 H 0 F 0",
-	"M 1440,0 H 1920 V 960 H 960 V 720 F 0",
-	"M 0,0 H 1080 L 840,240 H 0 F 0"
-}
+
+local ds = require ('level-1')
+
+
+
+
 
 local luapaths = {}
 
 for i, d in ipairs (ds) do
 	local luapath = svg2lua(d)
+	if luapath.bezier then
+		local curve = love.math.newBezierCurve(luapath)
+		luapath.curve = curve:render()
+	end
 	table.insert (luapaths, luapath)
 end
 
-
-function love.load()
-	love.window.setMode(1920, 1080, {resizable=true, borderless=false})
+local nodeMap = {}
+for i, line in ipairs (luapaths) do
+	if line.road then
+		local x1, y1 = line[1], line[2]
+		local x2, y2 = line[3], line[4]
+		local x3, y3 = line[#line-3], line[#line-2]
+		local x4, y4 = line[#line-1], line[#line]
+		if not nodeMap[x1] then nodeMap[x1] = {} end
+		if not nodeMap[x1][y1] then nodeMap[x1][y1] = {x=x1, y=y1, dx=0,dy=0} end
+		nodeMap[x1][y1].dx = nodeMap[x1][y1].dx + (x2-x1)
+		nodeMap[x1][y1].dy = nodeMap[x1][y1].dy + (y2-y1)
+		
+		if not nodeMap[x4] then nodeMap[x4] = {} end
+		if not nodeMap[x4][y4] then nodeMap[x4][y4] = {x=x4, y=y4, dx=0,dy=0} end
+		nodeMap[x4][y4].dx = nodeMap[x4][y4].dx + (x4-x3)
+		nodeMap[x4][y4].dy = nodeMap[x4][y4].dy + (y4-y3)
+	end
 end
 
+--local function normalization (dx, dy)
+--	local length = (dx*dx+dy*dy)^0.5
+--	if length > 0 then
+--		return dx/length, dy/length
+--	end
+--end
+
+local nodes = {}
+for x, ys in pairs (nodeMap) do
+	for y, node in pairs (ys) do
+--		local nx, ny = normalization (node.dx, node.dy)
+--		node.xn, node.ny = nx, ny
+		node.angle = math.atan2(node.dy, node.dx)
+		table.insert (nodes, node)
+	end
+end
+
+local arrowImage = love.graphics.newImage('graphics/arrow.png')
+
+local selectorPoint = nil
+
+local carsImage = love.graphics.newImage('graphics/cars_95x48.png')
+local carsQuads = {}
+for x=0, 760-1, 95 do
+	local quad = love.graphics.newQuad(x, 0, 95, 48, carsImage)
+	table.insert (carsQuads, quad)
+end
+
+local cars = {}
  
 function love.update(dt)
 	
 end
 
+local function drawBackground ()
+	love.graphics.setColor(bckGrcolor)
+	love.graphics.rectangle ('fill', 0,0, 1920, 960)
+	love.graphics.setLineWidth(1)
+	love.graphics.setColor(1,1,1,0.4)
+	for x = 0, 1920, 40 do
+		love.graphics.line (x, 0, x, 960)
+	end
+	for y = 0, 960, 40 do
+		love.graphics.line (0, y, 1920, y)
+	end
+	love.graphics.setColor(1,1,1,0.6)
+	for x = 0, 1920, 120 do
+		love.graphics.line (x, 0, x, 960)
+	end
+	for y = 0, 960, 120 do
+		love.graphics.line (0, y, 1920, y)
+	end
+end
 
-function love.draw()
-	for i, vertices in ipairs (luapaths) do
-		love.graphics.setLineWidth (40)
-		if #vertices > 2 then
-			if vertices.bezier then
-				if not vertices.curve then
-					local curve = love.math.newBezierCurve(vertices)
-					vertices.curve = curve:render()
-				end
-				love.graphics.line (vertices.curve)
-			elseif vertices.fill then
-				love.graphics.setLineWidth (1)
-				love.graphics.polygon('fill', vertices)
+local function drawRoads (lines)
+	love.graphics.setLineWidth (40)
+	love.graphics.setColor (roadColor)
+	for i, road in ipairs (lines) do
+		if road.road then 
+
+			if road.curve then
+				love.graphics.line (road.curve)
 			else
-				love.graphics.line (vertices)
+				love.graphics.line (road)
 			end
 		end
 	end
+	
+	love.graphics.setLineWidth (35)
+	love.graphics.setColor (lineColor)
+	for i, road in ipairs (lines) do
+		if road.road then 
+
+			if road.curve then
+				love.graphics.line (road.curve)
+			else
+				love.graphics.line (road)
+			end
+		end
+	end
+
+	love.graphics.setLineWidth (29)
+	love.graphics.setColor (roadColor)
+	for i, road in ipairs (lines) do
+		if road.road then 
+
+			if road.curve then
+				love.graphics.line (road.curve)
+			else
+				love.graphics.line (road)
+			end
+		end
+	end
+	
+	love.graphics.setLineWidth (1)
+	love.graphics.setColor (1,1,1,0.20)
+	for i, road in ipairs (lines) do
+		if road.road then 
+
+			if road.curve then
+				love.graphics.line (road.curve)
+			else
+				love.graphics.line (road)
+			end
+		end
+	end
+end
+
+local function drawBuildings (lines)
+	love.graphics.setLineWidth (2)
+	love.graphics.setColor (buildingColor)
+	for i, building in ipairs (lines) do
+		if building.fill then
+			
+			love.graphics.polygon('fill', building)
+		end
+	end
+end
+
+local function drawArrows ()
+	love.graphics.setColor(1,1,1)
+	local w, h = arrowImage:getDimensions ()
+	for i, node in ipairs (nodes) do
+		love.graphics.draw(arrowImage, node.x, node.y, node.angle, 0.65,0.65, w, h/2)
+	end
+end
+
+local function drawRotatedRectangle(mode, x, y, width, height, angle)
+	-- We cannot rotate the rectangle directly, but we
+	-- can move and rotate the coordinate system.
+	love.graphics.push()
+	love.graphics.translate(x, y)
+	love.graphics.rotate(angle)
+	love.graphics.rectangle(mode, -width/2, -height/2, width, height)
+	love.graphics.pop()
+end
+
+function drawTriangle (mode, x, y, length, width , angle) -- position, length, width and angle
+	love.graphics.push()
+	love.graphics.translate(x, y)
+	love.graphics.rotate( angle )
+	love.graphics.polygon(mode, -length/2, -width /2, -length/2, width /2, length/2, 0)
+	love.graphics.pop() 
+end
+
+local function drawSelectorPoint ()
+	if selectorPoint then
+		local x, y, angle = selectorPoint.x, selectorPoint.y, selectorPoint.angle
+		local width, height = 40, 20
+		drawRotatedRectangle('line', x, y, width, height, angle)
+		drawTriangle ('line', x, y, width, height, angle)
+	end
+end
+
+local function drawCars ()
+	love.graphics.setColor(1,1,1)
+	local w, h = carsImage:getDimensions()
+	w=w/8
+	for i, car in ipairs (cars) do
+		love.graphics.draw(carsImage, car.quad, car.x, car.y, car.angle, 0.6,0.6, w/2, h/2)
+	end
+end
+
+function love.draw()
+	mr.draw()
+	drawBackground ()
+	drawRoads (luapaths)
+	drawBuildings (luapaths)
+	drawArrows ()
+	
+	drawCars ()
+	
+	drawSelectorPoint ()
 	
 	local mx, my = love.mouse.getPosition()
 	love.graphics.print (mx..' '..my)
 end
 
+function love.resize (w, h)
+	mr.resize (w, h)
+end
+
 function love.keypressed(key, scancode, isrepeat)
+	mr.keypressed(key, scancode, isrepeat)
 	if false then
 	elseif key == "escape" then
 		love.event.quit()
@@ -85,15 +237,72 @@ end
 
 function love.mousepressed( x, y, button, istouch, presses )
 	if button == 1 then -- left mouse button
+		
 	elseif button == 2 then -- right mouse button
+		
 	end
 end
 
+local function distPointToLine(px,py,x1,y1,x2,y2) -- point, start and end of the segment
+	local dx,dy = x2-x1,y2-y1
+	local length = math.sqrt(dx*dx+dy*dy)
+	dx,dy = dx/length,dy/length
+	local p = dx*(px-x1)+dy*(py-y1)
+	if p < 0 then
+		dx,dy = px-x1,py-y1
+		return math.sqrt(dx*dx+dy*dy), x1, y1 -- distance, nearest point
+	elseif p > length then
+		dx,dy = px-x2,py-y2
+		return math.sqrt(dx*dx+dy*dy), x2, y2 -- distance, nearest point
+	end
+	return math.abs(dy*(px-x1)-dx*(py-y1)), x1+dx*p, y1+dy*p -- distance, nearest point
+end
+
+local function nearestSegmentInLine (x, y, line)
+	local x1, y1, x2, y2, min_dist
+	local nx, ny, px, py
+	local ax,ay = line[1], line[2]
+	for j = 3, #line-1, 2 do
+		local bx,by = line[j], line[j+1]
+		local dist, px,py = distPointToLine(x,y,ax,ay,bx,by)
+		if not min_dist or dist < min_dist then
+			min_dist = dist
+			x1, y1, x2, y2 = ax,ay,bx,by
+			nx,ny = px,py
+		end
+		ax, ay = bx, by
+	end
+	return x1, y1, x2, y2, nx, ny, min_dist -- segment, nearest point
+end
+
 function love.mousemoved( x, y, dx, dy, istouch )
+	local x,y = mr.getPosition()
+	selectorPoint = nil
+	local gap = 40
+	local point
+	for i, road in ipairs (luapaths) do
+		if road.road then
+			local line = road.curve or road
+			local x1, y1, x2, y2, nx, ny, dist = nearestSegmentInLine (x, y, line)
+			if dist < gap then
+				gap = dist
+				point = {x=nx, y=ny, angle = math.atan2(y2-y1, x2-x1)}
+			end
+		end
+	end
+	selectorPoint = point
 end
 
 function love.mousereleased( x, y, button, istouch, presses )
 	if button == 1 then -- left mouse button
+		if selectorPoint then
+			local carQuad = carsQuads[math.random(#carsQuads)]
+			local car = selectorPoint
+			car.quad = carQuad
+			table.insert (cars, car)
+			selectorPoint = nil
+		end
 	elseif button == 2 then -- right mouse button
+		cars[#cars] = nil
 	end
 end
---
+++ svg2lua/multiresolution.lua
@@ -0,0 +1,189 @@
+-- makes all window operations
+
+
+local multiresolution = {}
+local mr = multiresolution
+
+function mr.load()
+	-- if your code was optimized for fullHD:
+	mr.high_resolution = true
+	mr.editor = false
+	mr.editor_gap = 40
+	mr.translateX = 0
+	mr.translateY = 0
+	mr.scale = 1
+	-- mr.width, mr.height
+	mr.width = 1920
+	mr.height = 1080
+	local width, height = love.graphics.getDimensions ()
+	mr.resize (width, height) -- update new translation and scale
+	
+	mr.fonts = {}
+--	mr.fonts[10] = love.graphics.newFont('fonts/NotoSans-Regular.ttf', 10, "normal", 2)
+--	mr.fonts[20] = love.graphics.newFont('fonts/NotoSans-Regular.ttf', 20, "normal", 2)
+--	mr.fonts[40] = love.graphics.newFont('fonts/NotoSans-Regular.ttf', 40, "normal", 2)
+--	mr.fonts[80] = love.graphics.newFont('fonts/NotoSans-Regular.ttf', 80, "normal", 2)
+	mr.fonts[10] = love.graphics.newFont(10)
+	mr.fonts[20] = love.graphics.newFont(20)
+	mr.fonts[40] = love.graphics.newFont(40)
+	mr.fonts[80] = love.graphics.newFont(80)
+	love.graphics.setFont(mr.fonts[20])
+	
+	mr.create_background_canvas()
+	mr.create_hr_background_canvas()
+end
+
+function mr.update(dt)
+	if mr.need_update then
+		mr.resize()
+		mr.need_update = false
+	end
+end
+
+function mr.draw_canvas (is_hight_resolution)
+	love.graphics.setColor(1,1,1)
+	if is_hight_resolution then
+		love.graphics.draw(mr.hr_grid_background, 0, 0, 0, 1/mr.scale)
+	else
+		love.graphics.draw(mr.grid_background)
+	end
+end
+
+function mr.draw()
+	-- first translate, than scale
+	
+--	love.graphics.origin()
+	love.graphics.translate (mr.translateX, mr.translateY)
+	love.graphics.scale (mr.scale)
+
+	-- permanent background
+--	mr.draw_canvas (mr.high_resolution)
+	
+
+end
+
+
+
+--love.mouse.getPosition ()
+function mr.getPosition()
+	local mx = math.floor ((love.mouse.getX()-mr.translateX)/mr.scale+0.5)
+	local my = math.floor ((love.mouse.getY()-mr.translateY)/mr.scale+0.5)
+	return mx, my
+end
+
+--love.mouse.getX ()
+function mr.getX()
+	local mx = math.floor ((love.mouse.getX()-mr.translateX)/mr.scale+0.5)
+	return mx
+end
+
+--love.mouse.getY ()
+function mr.getY()
+	local my = math.floor ((love.mouse.getY()-mr.translateY)/mr.scale+0.5)
+	return my
+end
+
+--love.graphics.getDimensions( )
+function mr.getDimensions( )
+	return mr.width, mr.height
+end
+
+
+function mr.resize () -- update new translation and scale:
+	local w, h = love.graphics.getDimensions()
+	local w1, h1 = mr.width, mr.height -- target rendering resolution
+	local gap = 0
+	if mr.editor then
+		gap = mr.editor_gap
+	end
+	local scale = math.min (w/(w1+2*gap), h/(h1+2*gap))
+	mr.translateX = math.floor((w-w1*scale)/2+0.5)
+	mr.translateY = math.floor((h-h1*scale)/2+0.5)
+	mr.scale = scale
+	
+--	mr.create_background_canvas()
+	mr.create_hr_background_canvas()
+end
+
+function mr.set_to_update ()
+--	mr.editor = not mr.editor
+	mr.need_update = true
+end
+	
+	
+function mr.keypressed(key, scancode, isrepeat)
+	if key == "f11" then
+		mr.fullscreen = not mr.fullscreen
+		love.window.setFullscreen( mr.fullscreen)
+		mr.need_update = true
+	elseif key == "f10" then
+		mr.set_to_update ()
+	elseif key == "f9" then
+		mr.high_resolution = not mr.high_resolution
+	end
+	mr.resize ()
+end
+
+
+function mr.draw_grid (width, height, scale)
+	width, height = width*scale, height*scale
+	
+	love.graphics.setColor(89/255,157/255,220/255) -- 599DDC
+	love.graphics.rectangle('fill', 0, 0, width, height)
+	
+	local color1 = {177/255,201/255,236/255} -- 93bfe8
+	local color2 = {128/255,179/255,228/255} -- 76aee2
+	
+	local grid_size = 40*scale
+	love.graphics.setLineWidth (1)
+	-- color 2
+	love.graphics.setColor(color2)
+	for x = 0, width/grid_size do
+		local x1 = math.floor(x*grid_size+0.5)
+		love.graphics.line(x1, 0, x1, height)
+	end
+	for y = 0, height/grid_size do
+		local y1 = math.floor(y*grid_size+0.5)
+		love.graphics.line(0, y1, width, y1)
+	end
+	-- color 1
+	love.graphics.setColor(color1)
+	for x = 0, width/grid_size do
+		if (x%3 == 0) then
+			local x1 = math.floor(x*grid_size+0.5)
+			love.graphics.line(x1, 0, x1, height)
+		end
+	end
+	for y = 0, height/grid_size do
+		if (y%3 == 0) then
+			local y1 = math.floor(y*grid_size+0.5)
+			love.graphics.line(0, y1, width, y1)
+		end
+	end
+end
+
+function mr.create_background_canvas()
+	local width, height = mr.width, mr.height
+	local scale = mr.scale
+	mr.grid_background = love.graphics.newCanvas(width, height, {dpiscale = math.ceil(scale)})
+	love.graphics.push( )
+		love.graphics.origin()
+		love.graphics.setCanvas(mr.grid_background)
+			mr.draw_grid (width, height, 1)
+		love.graphics.setCanvas()
+	love.graphics.pop()
+end
+
+function mr.create_hr_background_canvas()
+	local width, height = mr.width, mr.height
+	local scale = mr.scale
+	mr.hr_grid_background = love.graphics.newCanvas(scale*width, scale*height)
+	love.graphics.push( )
+		love.graphics.origin()
+		love.graphics.setCanvas(mr.hr_grid_background)
+			mr.draw_grid (width, height, scale)
+		love.graphics.setCanvas()
+	love.graphics.pop()
+end
+
+return multiresolution
