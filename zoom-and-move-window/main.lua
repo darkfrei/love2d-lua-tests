5d968045edf155f37b7ebfbf9d74a32bfc48f612
darkfrei <34188677+darkfrei@users.noreply.github.com>
2022-01-25 23:48:57 +0100

    Add files via upload

--- zoom-and-move-window/main.lua
+++ zoom-and-move-window/main.lua
@@ -1,21 +1,24 @@
-window = require ("zoom-and-move-window")
+-- License CC0 (Creative Commons license) (c) darkfrei, 2022
+
+
+local Screen = require ("zoom-and-move-window")
 
 local line = require ("line") -- example curve
 
 function love.load()
-	window:load()
+	Screen:load()
 	
 end
 
 
 function love.update(dt)
-	window:update(dt)
+	
 	
 end
 
 
 function love.draw()
-	window:draw()
+	Screen:draw()
 
 	
 	
@@ -28,7 +31,7 @@ function love.draw()
 	love.graphics.line(0,-800,0,800)
 	love.graphics.line(-800,0,800,0)
 	love.graphics.print ('O', 2, -15)
---	love.graphics.print ('(O: ' .. window.translate.x .. ' ' .. window.translate.y .. ')', 2, 2)
+--	love.graphics.print ('(O: ' .. Screen.translate.x .. ' ' .. Screen.translate.y .. ')', 2, 2)
 	
 --	draws custom graphics
 	love.graphics.setColor(1,1,0)
@@ -42,32 +45,55 @@ function love.draw()
 	love.graphics.setColor(0,1,0)
 	love.graphics.print ('Debug GUI:', 1, 0)
 	love.graphics.print('x: ' .. mx ..', y: '.. my, 1, 15) 
-	love.graphics.print('x: ' .. (mx-window.translate.x)/window.zoom ..
-		', y: '.. (my-window.translate.y)/window.zoom, 1, 30) 
+	love.graphics.print('x: ' .. (mx-Screen.translate.x)/Screen.scale ..
+		', y: '.. (my-Screen.translate.y)/Screen.scale, 1, 30) 
 	
-	love.graphics.print('zoom ' .. window.zoom, 1, 45) 
-	love.graphics.print('tr ' .. window.translate.x ..', '.. window.translate.y, 1, 60) 
-	love.graphics.print('dscale: ' .. window.dscale , 1, 75)
+	love.graphics.print('zoom ' .. Screen.scale, 1, 45) 
+	love.graphics.print('tr ' .. Screen.translate.x ..', '.. Screen.translate.y, 1, 60) 
 end
-
-
---function love.mousepressed(x, y, k)
---	window:mousepressed(x, y, k)
---end
 
  
 function love.wheelmoved(x, y)
-	window:wheelmoved(x, y)
+	Screen:wheelmoved(x, y)
 end
 
-
-function love.mousepressed(x, y, button, istouch)
-	window:mousepressed(x, y, k)
+function love.mousepressed (x, y, button, istouch, presses)
+	Screen:mousepressed (x, y, button, istouch, presses)
+	
 end
 
+function love.mousemoved (x, y, dx, dy, istouch)
+	Screen:mousemoved (x, y, dx, dy, istouch)
+	
+end
 
+function love.mousereleased (x, y, button)
+	Screen:mousereleased (x, y, button)
+end
 
+function love.keypressed(key, scancode, isrepeat)
+	if false then
+	elseif key == "escape" then
+		love.event.quit()
+	end
+end
 
+-- touch screen
+
+function love.touchpressed (id, x, y, dx, dy, pressure)
+	Screen:touchpressed (id, x, y, dx, dy, pressure)
+	
+end
+
+function love.touchmoved (id, x, y, dx, dy, pressure)
+    Screen:touchmoved (id, x, y, dx, dy, pressure)
+	
+end
+
+function love.touchreleased (id, x, y, dx, dy, pressure)
+    Screen:touchreleased (id, x, y, dx, dy, pressure)
+	
+end
 
 
 
--- zoom-and-move-window/zoom-and-move-window.lua
+++ zoom-and-move-window/zoom-and-move-window.lua
@@ -1,72 +1,106 @@
-local zam_window = {}
+-- scale and move the window
+-- License CC0 (Creative Commons license) (c) darkfrei, 2022
 
 
-function zam_window:load()
+local Screen = {}
+
+
+function Screen:load()
+	self.x, self.y, self.w, self.h = love.window.getSafeArea()
 	self.translate={x=0, y=0}
-	self.delta={x=0, y=0}
-	self.zoom=1
+	self.scale=1
 	self.dscale = 2^(1/6)
 	self.mouse_pressed = false
+	-- for touchscreen
+	self.touches = {}
 end
 
-
-function zam_window:update(dt)
-	zam_window:update_translation ()
-end
-
-
-function zam_window:draw()
---	zam_window:update_translation ()
-
---	first translate than scale:
+function Screen:draw()
+	love.graphics.setColor(0,1,0)
+	love.graphics.setLineWidth(2)
+	love.graphics.rectangle('line', self.x, self.y, self.w, self.h)
 	love.graphics.translate(math.floor(self.translate.x+0.5), math.floor(self.translate.y))
-	love.graphics.scale(self.zoom)
+	love.graphics.scale(self.scale)
 end
 
 
-function zam_window:mousepressed(x, y, k)
-	if k == 3 then
---		resets screen
-		self.zoom = 1
+function Screen:mousepressed (x, y, button, istouch, presses)
+	if button == 1 then
+		self.mouse_pressed = true
+	elseif button == 3 then
+		-- middle mouse button: reset translate and scale
 		self.translate.x = 0
 		self.translate.y = 0
+		self.scale = 1
 	end
 end
 
-function zam_window:update_translation ()
-	local mx = love.mouse.getX()
-	local my = love.mouse.getY()
-	if love.mouse.isDown(1) then
-		if not window.mouse_pressed then -- click
-			window.mouse_pressed = true
-			window.delta.x = (window.translate.x-mx)/window.zoom
-			window.delta.y = (window.translate.y-my)/window.zoom
-		else
-			window.translate.x = mx + window.delta.x*window.zoom
-			window.translate.y = my + window.delta.y*window.zoom
-		end
-	else	-- left mouse not pressed
-		if window.mouse_pressed then 
-			window.mouse_pressed = false
-		end
+function Screen:mousemoved (x, y, dx, dy, istouch)
+	if self.mouse_pressed then
+		self.translate.x = self.translate.x + dx
+		self.translate.y = self.translate.y + dy
 	end
 end
 
+function Screen:mousereleased (x, y, button)
+	if button == 1 then
+		self.mouse_pressed = false
+	end
+end
 
-function zam_window:wheelmoved(x, y)
+function Screen:wheelmoved(x, y)
 	local mx = love.mouse.getX()
 	local my = love.mouse.getY()
     if not (y == 0) then -- mouse wheel moved up or down
---		zoom in to point or zoom out of point
+--		scale in to point or scale out of point
 		local mouse_x = mx - self.translate.x
 		local mouse_y = my - self.translate.y
-		local k = self.dscale^y
-		self.zoom = self.zoom*k
-		self.translate.x = math.floor(self.translate.x + mouse_x*(1-k)+0.5)
-		self.translate.y = math.floor(self.translate.y + mouse_y*(1-k)+0.5)
-	else
-		print ('wheel x: ' .. x .. ' y: ' .. y)
+		local dscale = self.dscale^y
+		self.scale = self.scale*dscale
+		self.translate.x = math.floor(self.translate.x + mouse_x*(1-dscale)+0.5)
+		self.translate.y = math.floor(self.translate.y + mouse_y*(1-dscale)+0.5)
     end
 end
 
-return zam_window
+-- multitouch touch screen
+-- touch screen
+
+-- https://love2d.org/wiki/love.touchpressed
+function Screen:touchpressed (id, x, y, dx, dy, pressure)
+	local newTouch = {id=id, x=x, y=y, dx=0, dy=0}
+	table.insert (self.touches, newTouch)
+end
+
+-- https://love2d.org/wiki/love.touchmoved
+function Screen:touchmoved (id, x, y, dx, dy, pressure)
+	if #self.touches == 1 then
+		-- translate
+		self.translate.x = self.translate.x + dx
+		self.translate.y = self.translate.y + dy
+		self.touches[1].x = x
+		self.touches[1].y = y
+	elseif #self.touches == 2 then
+		self.translate.x = self.translate.x + dx/2
+		self.translate.y = self.translate.y + dy/2
+		local sign = (self.touches[2].id == id) and 1 or -1
+		local dx1 = self.touches[2].x - self.touches[1].x
+		local dy1 = self.touches[2].y - self.touches[1].y
+		local dx2 = self.touches[2].x - self.touches[1].x + sign*dx
+		local dy2 = self.touches[2].y - self.touches[1].y + sign*dy
+		
+		local dscale = (dx2*dx2+dy2*dy2)^0.5/(dx1*dx1+dy1*dy1)^0.5
+		self.scale = self.scale*dscale
+		
+	end
+end
+
+-- https://love2d.org/wiki/love.touchreleased
+function Screen:touchreleased (id, x, y, dx, dy, pressure)
+	for i, touch in ipairs (self.touches) do
+		if touch.id == id then
+			table.remove (self.touches, id)
+		end
+	end
+end
+
+return Screen
