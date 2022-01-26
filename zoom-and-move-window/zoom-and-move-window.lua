4bd7a12e895bb0f3810898c289a765c6757bc860
darkfrei <34188677+darkfrei@users.noreply.github.com>
2022-01-26 21:57:28 +0100

    Add files via upload

--- zoom-and-move-window/zoom-and-move-window.lua
+++ zoom-and-move-window/zoom-and-move-window.lua
@@ -96,7 +96,7 @@ function Screen:touchmoved (id, x, y, dx, dy, pressure)
 		local x2, y2 = self.touches[2].x, self.touches[2].y
 		x, y=(x1+x2)/2, (y1+y2)/2
 		local dx1, dy1 = x2-x1, y2-y1
-		local sign = (self.touches[1].id == id) and 1 or -1
+		local sign = (self.touches[1].id == id) and -1 or 1
 		local dx2, dy2 = dx1+dx*sign, dy1+dy*sign
 		
 		local dscale = (dx2*dx2+dy2*dy2)^0.5/(dx1*dx1+dy1*dy1)^0.5
