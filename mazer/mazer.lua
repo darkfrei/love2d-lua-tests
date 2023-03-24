66b1d50e5baad863a122167b8577d3c71ca35f83
darkfrei <34188677+darkfrei@users.noreply.github.com>
2023-03-24 15:10:48 +0100

    Optimized maze algorithm
    First cell maze[1][1] will be always horizontal for platformer issues.

--- mazer/mazer.lua
+++ mazer/mazer.lua
@@ -28,44 +28,62 @@ local function opposite (sideName)
 	return oppositeTable[sideName]
 end
 
-local function visit(maze, x, y)
-	local directions = {{0, -1, "up"}, {0, 1, "down"}, {-1, 0, "left"}, {1, 0, "right"}}
-	for _, dir in ipairs(shuffle (directions)) do
-		local nx, ny = x + dir[1], y + dir[2]
-		local exist = maze[ny] and maze[ny][nx]
-		if exist and not (maze[ny][nx].visited) then
-			local sideName = dir[3]
-			maze[y][x][sideName] = false
-			local oppositeSide = opposite (sideName)
-			maze[ny][nx][oppositeSide] = false
-			maze[ny][nx].visited = true
-			visit(maze, nx, ny)
-		end
+local sideNames = {
+	up={dx=0, dy=-1}, 
+	down={dx=0, dy=1}, 
+	left={dx=-1, dy=0}, 
+	right={dx=1, dy=0}, 
+}
+
+local connectCell, visit
+
+function connectCell (maze, x, y, sideName, deep)
+	local dx, dy = sideNames[sideName].dx, sideNames[sideName].dy
+	local exist = maze[y+dy] and maze[y+dy][x+dx]
+	if not exist or (maze[y+dy][x+dx].visited) then
+		return
+	end
+	
+	maze[y][x][sideName] = false
+	local oppositeSide = opposite (sideName)
+	maze[y+dy][x+dx][oppositeSide] = false
+	maze[y+dy][x+dx].visited = true
+	if deep then
+		return visit(maze, x+dx, y+dy)
+	end
+end
+
+function visit(maze, x, y)
+	local directions = {"up", "down", "left", "right"}
+	for _, sideName in ipairs(shuffle (directions)) do
+		connectCell (maze, x, y, sideName, true)
 	end
 end
 
 function mazer.generateMaze(width, height, startX, startY)
 	local maze = {}
-  for y = 1, height do
-    maze[y] = {}
-    for x = 1, width do
-      local cell = {up=true, down=true, left=true, right=true, visited = false}
+	for y = 1, height do
+		maze[y] = {}
+		for x = 1, width do
+			local cell = {up=true, down=true, left=true, right=true, visited = false}
 			maze[y][x] = cell
-    end
-  end
-
-  startX = startX or love.math.random(width)
-  startY = startY or love.math.random(height)
-  maze[startY][startX].visited = true
-  visit(maze, startX, startY)
+		end
+	end
 	
+	-- connect second to first:
+	connectCell (maze, 2, 1, "left", false)
+	startX = startX or love.math.random(width)
+	startY = startY or love.math.random(height)
+	maze[startY][startX].visited = true
+	visit(maze, startX, startY)
+
 	for y, xs in ipairs (maze) do
 		for x, cell in ipairs (xs) do
 			cell.visited = nil
 		end
 	end
-  
-  return maze
+	
+	return maze
 end
 
 local function removeWallFromGrid (grid, x1, y1, x2, y2, wall)
@@ -128,10 +146,10 @@ end
 function mazer.drawMaze (maze)
 	for y, xs in ipairs (maze) do
 		for x, cell in ipairs (xs) do
-			if cell.right then	love.graphics.line (x,   y-1, x,   y) end
+			if cell.right then	love.graphics.line (x,	 y-1, x,	 y) end
 			if cell.left 	then	love.graphics.line (x-1, y-1, x-1, y) end
 			if cell.up 		then	love.graphics.line (x-1, y-1, x, y-1) end
-			if cell.down 	then	love.graphics.line (x-1, y,   x,   y) end
+			if cell.down 	then	love.graphics.line (x-1, y,	 x,	 y) end
 		end
 	end
 end
