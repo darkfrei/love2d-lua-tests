b354072030d77c12e04ffef890884df30038cefc
darkfrei <34188677+darkfrei@users.noreply.github.com>
2023-03-24 17:09:29 +0100

    added tiles-to-rectangles lib

--- mazer/main.lua
+++ mazer/main.lua
@@ -1,24 +1,48 @@
 --main.lua
 
 local mazer = require ('mazer')
+local getRectanglesFromMap = require ('tiles-to-rectangles')
+
 love.window.setTitle( 'mazer - lib for Love2D' )
 
 
 local maze = mazer.generateMaze(16,12,16,12)
-local grid = mazer.createGrid (maze, 2, 2)
+local grid = mazer.createGrid (maze, 1, 2) -- wall size, cell size
+
+local rectangles = getRectanglesFromMap (grid, true) -- map, wallValue
 
 function love.draw ()
 	-- draw tiles
+	--[[
 	love.graphics.setLineWidth(1)
 	love.graphics.setColor(0.25,0.25,0.25)
 	mazer.drawGrid (grid, 12, 'fill')
 	love.graphics.setColor(0.5,0.5,0.5)
 	mazer.drawGrid (grid, 12, 'line')
+	]]
+	
+	love.graphics.push()
+		love.graphics.scale(16)
+		love.graphics.setLineWidth(1/8)
+		
+		for i, r in ipairs (rectangles) do
+			love.graphics.setColor(0.5,0.5,0.5)
+			love.graphics.rectangle('fill', r.x-1, r.y-1, r.w, r.h)
+			love.graphics.setColor(1,1,1)
+			love.graphics.rectangle('line', r.x-1, r.y-1, r.w, r.h)
+		end
+	love.graphics.pop()
 	
 	-- draw lines
-	local scale = 48
-	love.graphics.scale (scale)
-	love.graphics.setLineWidth(1/scale)
-	love.graphics.setColor(1,1,1)
-	mazer.drawMaze (maze)
+	
+	--[[
+	love.graphics.push()
+		local scale = 48
+		love.graphics.scale (scale)
+		love.graphics.setLineWidth(1/scale)
+		love.graphics.setColor(1,1,1)
+		mazer.drawMaze (maze)
+	love.graphics.pop()
+	]]
+	
 end
---
+++ mazer/tiles-to-rectangles.lua
@@ -0,0 +1,84 @@
+-- map tiles to rectangles
+-- license cc0, darkfrei 2023
+
+
+local function isRectangle (hashMap, x0, y0, w, h)
+	for y = y0, y0+h-1 do
+		for x = x0, x0+w-1 do
+			if hashMap[y] then
+				if not hashMap[y][x] then
+					return false
+				end
+			else
+				return false
+			end
+		end
+	end
+	return true
+end
+
+local function newRectangle (hashMap, x, y)
+	local w, h = 1, 1
+	while true do
+		local right = isRectangle (hashMap, x+w, y, 1, h)
+		local rightT = isRectangle (hashMap, x+w, y-1, 1, h+2)
+		local down = isRectangle (hashMap, x, y+h, w, 1)
+		local downT = isRectangle (hashMap, x-1, y+h, w+2, 1)
+		local canDiag = right and down and hashMap[y+w][x+h]
+		if canDiag then
+			w, h = w+1, h+1
+		elseif right and not rightT then
+			w = w+1
+		elseif down and not downT then
+			h = h+1
+		else
+			for y0 = y, y+h-1 do
+				for x0 = x, x+w-1 do
+					hashMap[y0][x0] = false
+				end
+			end
+			return {x=x, y=y, w=w, h=h}
+		end
+	end
+end
+
+local function getRectanglesFromMap (map, wallValue)
+	local hashMap = {}
+	for y = 1, #map do
+		hashMap[y] = {}
+		for x = 1, #map[y] do
+			hashMap[y][x] = (map[y][x] == wallValue) -- true by 1
+		end
+	end
+	
+	local rectangles = {}
+	for y = 1, #hashMap do
+		for x = 1, #hashMap[y] do
+			if hashMap[y][x] then
+				local rectangle = newRectangle (hashMap, x, y)
+				table.insert (rectangles, rectangle)
+			end
+		end
+	end
+	return rectangles
+end
+
+
+
+-- example
+--[[
+local map = {
+	{0,1,1,1,0},
+	{1,0,1,0,1},
+	{1,1,0,1,1},
+	{1,0,1,0,1},
+	{0,1,1,1,0},
+}
+
+local rectangles = getRectanglesFromMap (map)
+for i, r in ipairs (rectangles) do
+	print (i, r.x, r.y, r.w, r.h)
+end
+--]]
+
+return getRectanglesFromMap
