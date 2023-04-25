d86ab7dc37a10a0f451a0ec969fc771c6fa8ce60
darkfrei <34188677+darkfrei@users.noreply.github.com>
2023-04-25 12:08:46 +0200

    Add files via upload

--- pixelfont/main.lua
+++ pixelfont/main.lua
@@ -13,30 +13,42 @@ function createFont (fontSize)
 	local font = love.graphics.newFont(fontSize, "mono")
 	font:setFilter("nearest")
 	love.graphics.setFont(font)
-
-	local tempCanvas = love.graphics.newCanvas ()
-	tempCanvas:setFilter( "nearest", "nearest")
-
-	local height = font:getHeight( )
+	
+	
 	local str = " abcdefghijklmnopqrstuvwxyz" ..
     "ABCDEFGHIJKLMNOPQRSTUVWXYZ0" ..
     "123456789.,!?-+/():;%&`'*#=[]\""
+	local width = 0 -- monowidth
+	
+	for i = 1, #str do
+		local char = string.sub (str, i, i)
+		local charWidth = font:getWidth( char )	
+		width = math.max (width, charWidth)
+	end
+	local canvasWidth = 2+(width+1)*(#str)
+	local height = font:getHeight( )
+
+--	print (canvasWidth, height)
+	local tempCanvas = love.graphics.newCanvas (canvasWidth, height)
+	tempCanvas:setFilter( "nearest", "nearest")
 
 	local x = 1
 
 	love.graphics.setCanvas (tempCanvas)
 	-- background
-		love.graphics.setColor (0,0,0)
-		love.graphics.rectangle ('fill', x,0, tempCanvas:getDimensions())
+	love.graphics.setColor (0,0,0)
+	love.graphics.rectangle ('fill', 0,0, canvasWidth+1, height)
+	
+	
 	
 	for i = 1, #str do
 		local char = string.sub (str, i, i)
-		local width = font:getWidth( char )
-		
+		local charWidth = font:getWidth( char )
+		local dx = (width-charWidth)/2
 		
 		drawSeparator (x-1, height)
 		love.graphics.setColor (1,1,1)
-		love.graphics.print (char, x-1,-1)
+		love.graphics.print (char, x+dx-1,-1)
 		drawSeparator (x-1, height)
 		x = x + width +1
 	end
@@ -81,7 +93,7 @@ function love.keypressed(key, scancode, isrepeat)
 		createFont (fontSize)
 	
 	elseif key == "s" then
-		Canvas:newImageData():encode("png","font-"..CanvasWidth.."-"..CanvasHeight..".png")
+		Canvas:newImageData():encode("png","font-"..CanvasWidth.."-"..CanvasHeight.."-mono.png")
 		love.system.openURL("file://"..love.filesystem.getSaveDirectory())
 	elseif key == "escape" then
 		love.event.quit()
