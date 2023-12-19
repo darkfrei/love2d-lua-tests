f45c3bf5fbe8c0e2642f2d7e9b00d90820af95de
darkfrei <34188677+darkfrei@users.noreply.github.com>
2023-12-19 13:57:14 +0100

    Added borders

--- voronoi-canvas/main_01.lua
+++ voronoi-canvas/main_01.lua
@@ -1,17 +1,6 @@
 -- the simplest way to do the voronoi
 
-love.window.setTitle ('Voronoi Canvas - Love2D, made by darkfrei')
-
 local function generateVoronoiCanvas (cells, width, height)
-	local function dot(ax, ay, bx, by)
-		return ax*bx + ay*by
-	end
-
-	local function normalize(vx, vy)
-		local length = math.sqrt(vx*vx + vy*vy)
-		return vx/length, vy/length
-	end
-
 	local points = {}
 	local canvas = love.graphics.newCanvas(width, height)
 	love.graphics.setCanvas(canvas)
@@ -20,28 +9,18 @@ local function generateVoronoiCanvas (cells, width, height)
 	for y = 1, height do
 		for x = 1, width do
 			local dmin, dmin2 = math.huge, math.huge
-			local site1x, site1y
-			local site2x, site2y
 			local color = {0,0,0}
 			for i, cell in ipairs (cells) do
 				local d = (cell.siteX-x)^2+(cell.siteY-y)^2
 				if d < dmin then
 					dmin2 = dmin
-					site2x, site2y = site1x, site1y
 					dmin = d
 					color = cell.color
-					site1x, site1y = cell.siteX, cell.siteY
 				elseif d < dmin2 then
 					dmin2 = d
-					site2x, site2y = cell.siteX, cell.siteY
 				end
 			end
-
-			local ax, ay = site1x-x, site1y-y
-			local bx, by = site2x-x, site2y-y
-			local nbx, nby = normalize(site2x-site1x, site2y-site1y)
-			local d = dot(ax+bx, ay+by, nbx, nby)
-			if d < 4 then
+			if math.abs (dmin2^0.5-dmin^0.5) < 1 then
 				color = {0,0,0}
 			end
 			table.insert (points, {x, y, color[1], color[2], color[3]})
