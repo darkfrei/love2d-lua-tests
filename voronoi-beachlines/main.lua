53a848bdb992a7c18ee15eb6d4abc0ef819c7202
darkfrei <34188677+darkfrei@users.noreply.github.com>
2024-03-29 20:11:48 +0100

    Bezier-curve as parabola

--- voronoi-beachlines/main.lua
+++ voronoi-beachlines/main.lua
@@ -1,10 +1,11 @@
+-- 2024-03-25
 
 frame = {x=50,y=50, w=700,h=500}
 
 vertices = {
-	200,200,
+	200,120,
 --	300,210, 
-	500,250, 
+	500,180, 
 }
 
 dirY = 220
@@ -24,41 +25,50 @@ getFocusParabolaRoots (5, 3, 5) -- focus x, y, directrix y
 
 ---------------------------------------------------------------------
 
-function getBezierControlPoint_focus_directrix(fx, fy, ax, ay, bx, by)
-	-- (x-h)^2=4*p*(y-k)
-	if (ay == by) then
-		-- exception: horizontal AB
-		local k = (fy + dirY) / 2
-		local h = fx
-		local cx = h
-		local cy = ay + 2*(k-ay)
-		return cx, cy
-		
-	else
---	[Axis direction](https://en.wikipedia.org/wiki/Parabola#Axis_direction)
-		local h = fx
-		local k = (fy + dirY) / 2
-		local cx = (ax+bx)/2
-		local f = (k-dirY)/2
-		
-		-- vertex
-		-- h = -b/(2*a)
-		-- k = (4*a*c-b*b)/(4*a)
-		
-		local a = -1/(dirY-ay)
-		local b = -2 * a * h
-		local c = h * h * a + k
---		print ('h', h, -b/(2*a)) -- ok, same
---		print ('k', k, (4*a*c-b*b)/(4*a)) -- ok, same
-		
-		-- derivative value of parabola in point A (ax):
-		local day = a*2*ax + b
-		local cy = ay + day * (bx-ax)/2
-		
-		-- usage: three control points to draw parabola:
-		-- {ax, ay, cx, cy, bx, by}
-		return cx, cy
+
+
+function fdParabolaToBezier(fx, fy, dirY, ax, bx)
+	local f = function (x, fx, fy, dirY)
+		local n = x*x-2*fx*x+fx*fx+fy*fy-dirY*dirY
+		return n / (2*(fy-dirY))
 	end
+
+	local function df(x, fx, fy, dirY)
+		local derivative = (x-fx) / (fy-dirY)
+		return derivative
+	end
+
+	if (fy == dirY) then return end
+
+	-- y coordinate for point A:
+	local ay = f(ax, fx, fy, dirY)
+	-- tangent slope for A:
+	local ad = df(ax, fx, fy, dirY)
+	-- difference x for C and A
+	local dx = (bx-ax)/2
+	-- position of point C:
+	local cx = ax+dx
+	local cy = ay+dx*ad	
+	-- y coordinate for point B:
+	local by = f(bx, fx, fy, dirY)
+	return ax, ay, cx, cy, bx, by
+end
+
+-- fx, fy, dirY, ax, bx
+print (fdParabolaToBezier(0, 0.25, -0.25, -1, 2))
+
+function getBezierControlPoint (fx, fy, ax, bx)
+	local f = function (x)
+		return (x*x-2*fx*x+fx*fx+fy*fy-dirY*dirY) / (2*(fy-dirY))
+	end
+	local function df(x)
+		return (x-fx) / (fy-dirY)
+	end
+	if (fy == dirY) then return end -- not parabola
+	local ay, by = f(ax), f(bx)
+	local ad = df(ax) -- tangent slope for A
+	local dx = (bx-ax)/2
+	return ax+dx, ay+ad*dx
 end
 
 function evaluateParabola (fx, fy, x)
@@ -95,7 +105,7 @@ function updateBeachlines ()
 				ax = ax1
 				ay = ay1
 			end
-			
+
 			local bx, by = right_x, frame.y
 			if bx > frame.x + frame.w then
 				local bx1 = frame.x + frame.w
@@ -107,9 +117,9 @@ function updateBeachlines ()
 				bx = bx1
 				by = by1
 			end
-			
+
 --			local cx, cy = getBezierThirdControlPoint(fx, fy, ax, ay, bx, by+1)
-			local cx, cy = getBezierControlPoint_focus_directrix(fx, fy, ax, ay, bx, by)
+			local cx, cy = getBezierControlPoint(fx, fy, ax, bx)
 			beachLine.controlPoints = {
 				ax, ay,
 				cx, cy,
@@ -144,18 +154,18 @@ function love.draw ()
 		love.graphics.line (beachLine.line)
 
 		if #beachLine.controlPoints > 3 
-			and beachLine.controlPoints[3] ~= nil then
+		and beachLine.controlPoints[3] ~= nil then
 			love.graphics.setColor (0,0.7,0,0.7)
 			love.graphics.setLineWidth (1)
 			love.graphics.line (beachLine.controlPoints)
 		end
-		
+
 		if beachLine.bezierLine then
-		love.graphics.setColor (0,0.7,0,0.7)
-		love.graphics.setLineWidth (1)
-		love.graphics.line (beachLine.bezierLine)
-	
-	end
+			love.graphics.setColor (0,0.7,0,0.7)
+			love.graphics.setLineWidth (1)
+			love.graphics.line (beachLine.bezierLine)
+
+		end
 
 	end
 end
